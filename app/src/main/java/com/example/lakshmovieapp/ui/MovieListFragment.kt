b73264d96df321a4ee2564530111dde1c38b2b02
LakshLathiya <lakshlathiya@gmail.com>
2022-09-11 02:40:47 +0530

    First PR

--- app/src/main/java/com/example/lakshmovieapp/ui/MovieListFragment.kt
+++ app/src/main/java/com/example/lakshmovieapp/ui/MovieListFragment.kt
@@ -5,15 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.lakshmovieapp.databinding.FragmentMovieListBinding
 import com.example.lakshmovieapp.paging.LoaderAdapter
 import com.example.lakshmovieapp.paging.MoviePagingAdapter
 import com.example.lakshmovieapp.viewmodel.MovieViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -55,6 +60,21 @@ class MovieListFragment : Fragment() {
         movieViewModel.list.observe(viewLifecycleOwner, Observer {
             moviePagingAdapter.submitData(lifecycle, it)
         })
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            moviePagingAdapter.loadStateFlow.collectLatest { loadStates ->
+                binding.shimmerViewContainer.apply {
+                    startShimmerAnimation()
+                    isVisible = loadStates.refresh is LoadState.Loading
+                }
+                binding.layoutError.apply {
+                    root.isVisible = loadStates.refresh is LoadState.Error
+                    btnRetry.setOnClickListener {
+                        moviePagingAdapter.retry()
+                    }
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
