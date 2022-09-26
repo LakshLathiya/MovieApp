c88622c44b5fbf71dcd26eebaf82cffb191c3eed
LAKSH LATHIYA <lakshlathiya@gmail.com>
2022-09-26 14:35:45 +0530

    Add MovieDetails

--- app/src/main/java/com/example/lakshmovieapp/model/movieDetails/MovieDetails.kt
+++ app/src/main/java/com/example/lakshmovieapp/model/movieDetails/MovieDetails.kt
@@ -6,6 +6,6 @@ data class MovieDetails(
     val poster_path: String? = null,
     val release_date: String? = null,
     val title: String? = null,
-    val id: Int? = null,
+    val id: Int? = 0,
     val vote_average: Double? = null,
 )
